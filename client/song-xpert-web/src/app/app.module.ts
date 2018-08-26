640c307b4efbf14b521110e82b19e8366704cefb
tan-eddie <eddie010tan@gmail.com>
2018-08-26 17:55:37 +1000

    Add song component for playing audio

--- client/song-xpert-web/src/app/app.module.ts
+++ client/song-xpert-web/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
+import { SongComponent } from './song/song.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    SongComponent
   ],
   imports: [
     BrowserModule
